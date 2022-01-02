c405acaca1f8850d1fb664453b278fb07b389a0a
akkalame <akkalameo.o@gmail.com>
2022-01-02 09:25:10 -0400

    mejora de roadmap y header

--- js/index.js
+++ js/index.js
@@ -1,17 +1,50 @@
 $(document).ready( function(){
-  //Comprueba el scroll de la pantalla
-  //muestra el elemento para hacer top en la pagina
-  window.onscroll = function() {
-    let header = $('#mainheader');
-    let r = esVisible(header);
 
-    if (r){
-      $('#topscroll').hide();
+  menuMovil();
+
+  responsiveDomino();
+
+  addCheckRoadmap();
+
+  ocultarTopScroll();
+
+
+  
+});
+
+function esVisible(elem){
+    /* Ventana de Visualización*/
+    var posTopView = $(window).scrollTop();
+    var posButView = posTopView + $(window).height();
+    /* Elemento a validar*/
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    /* Comparamos los dos valores tanto del elemento como de la ventana*/
+    return ((elemBottom < posButView && elemBottom > posTopView) || (elemTop >posTopView && elemTop< posButView));
+}
+
+//cambia el tamaño de los domino dependiendo de las pantallas
+function responsiveDomino(){
+
+  if ( $(window).width() <= 1050 ) {
+    $('.domino-container .domino').addClass('sm');
+  }else{
+    $('.domino-container .domino').removeClass('sm');
+  }
+
+  $(window).resize(function(event) {
+    if ( $(window).width() <= 1050 ) {
+      $('.domino-container .domino').addClass('sm');
     }else{
-      $('#topscroll').show();
+      $('.domino-container .domino').removeClass('sm');
     }
-  };
 
+  });
+  
+}
+
+
+function menuMovil(){
   /* abrir y cerrar menu*/
   $('.btn-open').click(function() {
     $('nav').removeClass('close');
@@ -29,15 +62,28 @@ $(document).ready( function(){
     $('nav').removeClass('open');
     $('nav').addClass('close');
   });
-});
+}
 
-function esVisible(elem){
-    /* Ventana de Visualización*/
-    var posTopView = $(window).scrollTop();
-    var posButView = posTopView + $(window).height();
-    /* Elemento a validar*/
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-    /* Comparamos los dos valores tanto del elemento como de la ventana*/
-    return ((elemBottom < posButView && elemBottom > posTopView) || (elemTop >posTopView && elemTop< posButView));
+function addCheckRoadmap(){
+  $('.roadmapBox.right .stage-way.marked').append(' <i class="fas fa-check"></i>')
+  $('.roadmapBox.txt-right .stage-way.marked').prepend(' <i class="fas fa-check"></i> ')
+
+}
+
+function ocultarTopScroll(){
+  //Comprueba el scroll de la pantalla
+  //muestra el elemento para hacer top en la pagina
+  let topscroll = $('#topscroll');
+
+  $(window).scroll(function() {
+    
+    if ( $(window).scrollTop() < 1) {
+      topscroll.removeClass('visible');
+      topscroll.addClass('oculto');
+    }else{
+      topscroll.removeClass('oculto');
+      topscroll.addClass('visible');
+    }
+
+  });
 }
