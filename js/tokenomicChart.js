5f97e7e42600253f011b86829b36928b4f5a3073
akkalame <akkalameo.o@gmail.com>
2022-02-14 12:53:55 -0400

    cambio en tokenomic, cambio en el team

--- js/tokenomicChart.js
+++ js/tokenomicChart.js
@@ -3,7 +3,7 @@ const myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {        datasets: [{
             label: '',
-            data: [960000, 3600000, 3000000, 1800000, 480000, 1200000, 960000],
+            data: [960000, 3600000, 3000000, 3000000, 480000, 960000],
             backgroundColor: [
                 'rgba(17, 43, 80, 1)',
                 'rgba(20, 93, 136, 1)',
@@ -11,8 +11,7 @@ const myChart = new Chart(ctx, {
                 'rgba(249, 149, 35, 1)',
                 'rgba(123, 76, 130, 1)',
                 'rgba(57, 64, 155, 1)'
-                ,
-                'rgba(213, 115, 3, 1)'
+                
             ],
             borderColor: [
                 'rgba(255, 255, 255, 1)',
